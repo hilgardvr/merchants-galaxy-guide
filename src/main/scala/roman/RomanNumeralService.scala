aa792af5a28cd42cd092ac3c9fea12241c3275c3
Hilgard van Rooyen <hilgardvr@gmail.com>
2021-12-08 07:40:06 +0200

    init

---
+++ src/main/scala/roman/RomanNumeralService.scala
@@ -0,0 +1,15 @@
+package roman
+
+object RomanNumeralService {
+  val RomanNumerals = Map(
+    "I" -> 1,
+    "V" -> 5,
+    "X" -> 10,
+    //"L" -> 250,
+    "L" -> 50,
+    "C" -> 100,
+    "D" -> 500,
+    "M" -> 1000
+  )
+
+}
